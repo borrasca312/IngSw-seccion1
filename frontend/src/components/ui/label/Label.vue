a05116ac07b2d6ecfef8be460e58e274400a8697
Kingshiba204 <228562973+Nachito-King@users.noreply.github.com>
2025-10-26 23:34:05 -0300

    Update Label.vue

--- frontend/src/components/ui/label/Label.vue
+++ frontend/src/components/ui/label/Label.vue
@@ -1,21 +1,15 @@
-<script setup lang="ts">
-import { Label, type LabelProps } from 'radix-vue'
-import { useAttrs } from 'vue'
-import { cn } from '@/lib/utils'
-
-const props = defineProps<LabelProps>()
-
-const attrs = useAttrs()
-</script>
-
 <template>
-  <Label
-    v-bind="props"
+  <label
     :class="cn(
       'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
-      attrs.class ?? '',
+      $attrs.class
     )"
+    v-bind="$attrs"
   >
     <slot />
-  </Label>
+  </label>
 </template>
+
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+</script>
