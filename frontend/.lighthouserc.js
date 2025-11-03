ca829d2f3100c8368e166913a9553c20ef40a117
Kingshiba204 <228562973+Nachito-King@users.noreply.github.com>
2025-11-03 01:47:50 -0300

    Create .lighthouserc.js

---
+++ frontend/.lighthouserc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:4173'],
+      startServerCommand: 'npm run preview',
+      startServerReadyPattern: 'Local:',
+      numberOfRuns: 3,
+    },
+    assert: {
+      budgetPath: '../performance-budget.json',
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.8 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.8 }],
+        'first-contentful-paint': ['error', { maxNumericValue: 2000 }],
+        'largest-contentful-paint': ['error', { maxNumericValue: 2500 }],
+        'cumulative-layout-shift': ['error', { maxNumericValue: 0.1 }],
+        'total-blocking-time': ['error', { maxNumericValue: 300 }],
+        'speed-index': ['error', { maxNumericValue: 3000 }],
+        'interactive': ['error', { maxNumericValue: 3500 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
